474de21a205b9b9880f4eaac10663d8c7aedaa64
tawilkinson <tom@tawilkinson.com>
2020-10-04 13:36:38 +0100

    Autopep8

--- python/agenda.py
+++ python/agenda.py
@@ -14,26 +14,31 @@ class Agenda(commands.Cog):
     def scrape_events_from_calender(self):
         adgenda_html = requests.get(self.calendar)
         soup = BeautifulSoup(adgenda_html.text, 'html.parser')
-        adgenda_events = soup.select("body > div.view-container-border > div > div")
+        adgenda_events = soup.select(
+            "body > div.view-container-border > div > div")
         events = []
         for event in adgenda_events:
             event_text = event.text
-            oneline_event_text = event_text.replace('\n', ' ').replace('\r', '')
+            oneline_event_text = event_text.replace(
+                '\n', ' ').replace('\r', '')
             events.append(oneline_event_text)
         return events
 
     def format_full_schedule(self, schedule):
         return ',\n'.join(schedule)
 
-    @commands.command(name="cal", help="Opens a webpage to show the boardgame weekend calander of events!")
+    @commands.command(name="cal",
+                      help="Opens a webpage to show the boardgame weekend calander of events!")
     async def cal(self, ctx):
         await ctx.send(f'Board game festival schedule of events available online here: {self.sharable_calander}')
 
-    @commands.command(name="agenda", help="Prints the upcoming schedule for the weekend")
+    @commands.command(name="agenda",
+                      help="Prints the upcoming schedule for the weekend")
     async def agenda(self, ctx):
         schedule = self.scrape_events_from_calender()
         response = self.format_full_schedule(schedule)
         await ctx.send(response)
+
 
 def setup(bot):
     bot.add_cog(Agenda(bot))
