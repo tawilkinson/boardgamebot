c5fe143dc0eb3dd6405040f07f8503e9c6e2c79d
tawilkinson <tom@tawilkinson.com>
2020-10-04 15:22:34 +0100

    Embed for Agenda

--- python/agenda.py
+++ python/agenda.py
@@ -9,35 +9,55 @@ class Agenda(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.calendar = 'https://calendar.google.com/calendar/u/0/htmlembed?src=c5ilkhfkd424ddm47unrfuvd9c@group.calendar.google.com&amp;ctz=Europe/London&amp;mode=AGENDA&mode=AGENDA'
-        self.sharable_calander = 'https://calendar.google.com/calendar/embed?src=c5ilkhfkd424ddm47unrfuvd9c%40group.calendar.google.com'
+        self.sharable_calender = 'https://calendar.google.com/calendar/embed?src=c5ilkhfkd424ddm47unrfuvd9c%40group.calendar.google.com'
 
     def scrape_events_from_calender(self):
-        adgenda_html = requests.get(self.calendar)
-        soup = BeautifulSoup(adgenda_html.text, 'html.parser')
-        adgenda_events = soup.select(
-            "body > div.view-container-border > div > div")
+        agenda_html = requests.get(self.calendar)
+        soup = BeautifulSoup(agenda_html.text, 'html.parser')
+        agenda_events = soup.select(
+            'body > div.view-container-border > div > div')
         events = []
-        for event in adgenda_events:
-            event_text = event.text
-            oneline_event_text = event_text.replace(
-                '\n', ' ').replace('\r', '')
-            events.append(oneline_event_text)
+        for event in agenda_events:
+            date = event.select_one('div.date').text
+            event_classes = event.select('tr.event')
+            event_data = {}
+            event_data['date'] = date
+            event_data['events'] = []
+
+            for event_class in event_classes:
+                time = event_class.select_one('td.event-time').text
+                description = event_class.select_one(
+                    'span.event-summary').text
+                data = {}
+                data['time'] = time
+                data['description'] = description
+                event_data['events'].append(data)
+
+            events.append(event_data)
         return events
 
     def format_full_schedule(self, schedule):
-        return ',\n'.join(schedule)
+        embed = discord.Embed(title='Board Game Festival Agenda')
+        for date in schedule:
+            name = date['date']
+            value = ''
 
-    @commands.command(name="cal",
-                      help="Opens a webpage to show the boardgame weekend calander of events!")
+            for event in date['events']:
+                value += event['time'] + ' - ' + event['description'] + '\n'
+            embed.add_field(name=name, value=value, inline=True)
+        return embed
+
+    @commands.command(name='cal',
+                      help='Opens a webpage to show the boardgame weekend calender of events!')
     async def cal(self, ctx):
-        await ctx.send(f'Board game festival schedule of events available online here: {self.sharable_calander}')
+        await ctx.send(f'Board Game Festival schedule of events is available online here: {self.sharable_calender}')
 
-    @commands.command(name="agenda",
-                      help="Prints the upcoming schedule for the weekend")
+    @commands.command(name='agenda',
+                      help='Prints the upcoming schedule for the weekend')
     async def agenda(self, ctx):
         schedule = self.scrape_events_from_calender()
         response = self.format_full_schedule(schedule)
-        await ctx.send(response)
+        await ctx.send(embed=response)
 
 
 def setup(bot):
