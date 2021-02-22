747a2c5a7889d3ce0456684baf74d01eb0a293e1
Tom Wilkinson <tom@tawilkinson.com>
2021-02-22 00:04:24 +0000

    Automated autopep8 fixes

--- bot/python/help.py
+++ bot/python/help.py
@@ -25,29 +25,34 @@ class Help(commands.Cog):
         """Gets all cogs and commands of this bot."""
         try:
             if not cog:
-                halp = discord.Embed(title='Cog Listing and Uncatergorized Commands',
-                                     description='Use `bg help *cog*` to find out more about them!\n\
+                halp = discord.Embed(
+                    title='Cog Listing and Uncatergorized Commands',
+                    description='Use `bg help *cog*` to find out more about them!\n\
                                          (BTW, the Cog Name Must Be in Title Case, Just Like this Sentence.)\
                                              \n\nClick on ✉ to get this info via DM.',
-                                     color=discord.Color.blurple())
+                    color=discord.Color.blurple())
                 cogs_desc = ''
                 for x in self.bot.cogs:
                     cogs_desc += ('{} - {}'.format(x,
-                                                   self.bot.cogs[x].__doc__)+'\n')
-                halp.add_field(
-                    name='Cogs', value=cogs_desc[0:len(cogs_desc)-1], inline=False)
+                                                   self.bot.cogs[x].__doc__) + '\n')
+                halp.add_field(name='Cogs',
+                               value=cogs_desc[0:len(cogs_desc) - 1],
+                               inline=False)
                 cmds_desc = ''
                 for y in self.bot.walk_commands():
                     if not y.cog_name and not y.hidden:
-                        cmds_desc += ('{} - {}'.format(y.name, y.help)+'\n')
+                        cmds_desc += ('{} - {}'.format(y.name, y.help) + '\n')
                 halp.add_field(name='Uncatergorized Commands',
-                               value=cmds_desc[0:len(cmds_desc)-1], inline=False)
+                               value=cmds_desc[0:len(cmds_desc) - 1],
+                               inline=False)
                 message = await ctx.send('', embed=halp)
                 await message.add_reaction(emoji='✉')
             else:
                 if len(cog) > 1:
                     halp = discord.Embed(
-                        title='Error!', description='That is way too many cogs!', color=discord.Color.red())
+                        title='Error!',
+                        description='That is way too many cogs!',
+                        color=discord.Color.red())
                     await ctx.send('', embed=halp)
                 else:
                     found = False
@@ -55,7 +60,7 @@ class Help(commands.Cog):
                         for y in cog:
                             if x == y:
                                 halp = discord.Embed(
-                                    title=cog[0]+' Command Listing', description=self.bot.cogs[cog[0]].__doc__,
+                                    title=cog[0] + ' Command Listing', description=self.bot.cogs[cog[0]].__doc__,
                                     color=discord.Color.blurple())
                                 for c in self.bot.get_cog(y).get_commands():
                                     if not c.hidden:
@@ -64,7 +69,9 @@ class Help(commands.Cog):
                                 found = True
                     if not found:
                         halp = discord.Embed(
-                            title='Error!', description='How do you even use "'+cog[0]+'"?', color=discord.Color.red())
+                            title='Error!',
+                            description='How do you even use "' + cog[0] + '"?',
+                            color=discord.Color.red())
 
                     message = await ctx.send('', embed=halp)
                     await message.add_reaction(emoji='✉')
