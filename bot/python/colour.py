fd4c05940c881488aed6a8a8295b6cf2e03f4706
tawilkinson <tom.wilkinson@mrsolutions.com>
2021-04-01 18:04:39 +0100

    enumerate()

--- bot/python/colour.py
+++ bot/python/colour.py
@@ -34,13 +34,13 @@ def get_dominant_colour(img_url, debug=False):
 
     percent = []
     _, counts = np.unique(labels, return_counts=True)
-    for i in range(len(centroid)):
+    for _, i in enumerate(len(centroid)):
         j = counts[i]
         j = j / (len(labels))
         percent.append(j)
 
     if debug:
-        for i in range(len(centroid)):
+        for _, i in enumerate(len(centroid)):
             print(centroid[i], '{:0.2f}%'.format(percent[i] * 100))
     indices = np.argsort(percent)[::-1]
     dominant = centroid[indices[0]]
