587c30b4a056a304dcc35eb7e2af009051b81c77
tawilkinson <tom.wilkinson@mrsolutions.com>
2021-04-01 18:02:42 +0100

    Fix help comment

--- bot/python/help.py
+++ bot/python/help.py
@@ -1,8 +1,8 @@
 import discord
 from discord.ext import commands
-'''This custom help command is a perfect replacement for the default one on any Discord Bot written in Discord.py Rewrite!
 
-Inspired by Jared Newsom (AKA Jared M.F.) - https://gist.github.com/That-Kidd/432b028352a44e434dfd54e3676a6a85'''
+# This custom help command is a perfect replacement for the default one on any Discord Bot written in Discord.py Rewrite!
+# Inspired by Jared Newsom (AKA Jared M.F.) - https://gist.github.com/That-Kidd/432b028352a44e434dfd54e3676a6a85
 
 
 class Help(commands.Cog, name='help'):
