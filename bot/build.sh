7d07bd174f3350b45f96fc9c2859dc7e251f89fb
tawilkinson <tom@tawilkinson.com>
2021-09-01 18:16:07 +0100

    refactor!: dockerised typescript version building

--- bot/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-. project.cfg
-
-docker build \
-  --tag="$repo:latest" \
-  --tag="$repo:$tag" \
-  .
--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,11 +1,56 @@
-import { ApplyOptions } from '@sapphire/decorators';
-import { Events, Listener, ListenerOptions } from '@sapphire/framework';
+import type { ListenerOptions, PieceContext } from '@sapphire/framework';
+import { Listener, Store } from '@sapphire/framework';
+import { blue, gray, green, magenta, magentaBright, white, yellow } from 'colorette';
 
-@ApplyOptions<ListenerOptions>({ once: true })
-export default class extends Listener<typeof Events.ClientReady> {
+const dev = process.env.NODE_ENV !== 'production';
 
-	public run() {
-		return this.container.client.logger.info(`Logged in as ${this.container.client.user!.tag} and ready to serve ${this.container.client.guilds.cache.size} guilds!`);
+export class UserEvent extends Listener {
+	private readonly style = dev ? yellow : blue;
+
+	public constructor(context: PieceContext, options?: ListenerOptions) {
+		super(context, {
+			...options,
+			once: true
+		});
 	}
 
+	public run() {
+		this.printBanner();
+		this.printStoreDebugInformation();
+	}
+
+	private printBanner() {
+		const success = green('+');
+
+		const llc = dev ? magentaBright : white;
+		const blc = dev ? magenta : blue;
+
+		const line01 = llc('');
+		const line02 = llc('');
+		const line03 = llc('');
+
+		// Offset Pad
+		const pad = ' '.repeat(7);
+
+		console.log(
+			String.raw`
+${line01} ${pad}${blc('1.0.0')}
+${line02} ${pad}[${success}] Gateway
+${line03}${dev ? ` ${pad}${blc('<')}${llc('/')}${blc('>')} ${llc('DEVELOPMENT MODE')}` : ''}
+		`.trim()
+		);
+	}
+
+	private printStoreDebugInformation() {
+		const { client, logger } = this.container;
+		const stores = [...client.stores.values()];
+		const last = stores.pop()!;
+
+		for (const store of stores) logger.info(this.styleStore(store, false));
+		logger.info(this.styleStore(last, true));
+	}
+
+	private styleStore(store: Store<any>, last: boolean) {
+		return gray(`${last ? '└─' : '├─'} Loaded ${this.style(store.size.toString().padEnd(3, ' '))} ${store.name}.`);
+	}
 }
