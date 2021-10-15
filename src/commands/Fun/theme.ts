ecf7f6813cdd3f68ec68234fa75da58d0a0e2781
Tom Wilkinson <tom@tawilkinson.com>
2021-10-15 03:46:42 +0100

    feat(refactor): theme.ts to replace fun.py

---
+++ src/commands/Fun/theme.ts
@@ -0,0 +1,20 @@
+import { Command, CommandOptions, PieceContext } from '@sapphire/framework';
+import type { Message } from 'discord.js';
+import { Styles, Components, Settings } from '../../lib/theme_data';
+
+export class UserCommand extends Command {
+	public constructor(context: PieceContext, options: CommandOptions) {
+		super(context, {
+			...options,
+			description: 'Generates a random board game theme.',
+			aliases: ['t']
+		});
+	}
+
+	public async run(message: Message) {
+		const style = Styles[Math.floor(Math.random() * Styles.length)];
+		const component = Components[Math.floor(Math.random() * Components.length)];
+		const setting = Settings[Math.floor(Math.random() * Settings.length)];
+		return message.channel.send(`${style} using ${component} set in ${setting}`);
+	}
+}
---
+++ src/lib/theme_data.ts
@@ -0,0 +1,49 @@
+export const Styles: string[] = [
+	'Eurogame',
+	'Card game',
+	'Roll & Write',
+	'Strategy game',
+	'Worker placement',
+	'Engine builder',
+	'4X',
+	'Deck builder',
+	'Abstract game',
+	'Dexterity game',
+	'Drafting game',
+	'Roll & move',
+	'Push-your-luck',
+	'Social deduction'
+];
+
+export const Components: string[] = [
+	'meeples',
+	'legacy mechanics',
+	'tableau',
+	'area control',
+	'hidden roles',
+	'deck building',
+	'rondels',
+	'storytelling',
+	'trick-taking',
+	'hand management',
+	'victory points',
+	'tile placement',
+	'drafting',
+	'custom dice'
+];
+
+export const Settings: string[] = [
+	'Istanbul',
+	'Carcassonne',
+	'London',
+	'America',
+	'a sushi restaurant',
+	'Tokyo',
+	'Warsaw',
+	'a space ship',
+	'the bottom of the ocean',
+	'Birmingham',
+	'a factory',
+	'the world of Warhammer Fantasy Battles',
+	'Waterdeep'
+];
