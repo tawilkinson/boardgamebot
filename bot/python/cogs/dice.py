064822f62278b01086eade8e07033057bf67cae5
tawilkinson <tom@tawilkinson.com>
2023-06-14 00:48:49 +0100

    feat: slash commands for dice.py

--- bot/python/cogs/dice.py
+++ bot/python/cogs/dice.py
@@ -1,4 +1,6 @@
 import logging
+import discord
+from discord import app_commands
 from discord.ext import commands
 from utils.roller import Roller
 
@@ -11,29 +13,26 @@ class Dice(commands.Cog, name='dice'):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(
-        aliases=[
-            'r',
-            'dieroll'],
-        help='Using standard dice notation: You can roll up to 9,999 dice with up to 9,999 sides each.\n\
+    @app_commands.command(name='roll', description='Roll up to 9,999 dice with up to 9,999 sides each')
+    async def roll(self, interaction: discord.Interaction, roll_text: str) -> None:
+        '''
+        Using standard dice notation: You can roll up to 9,999 dice with up to 9,999 sides each.\n\
         Examples:\n- `bg roll <x>d20`: rolls <x> twenty sided die.\n- `bg roll 2d20kl1`: rolls 2 d20 and keeps \
             the lowest result, i.e. disadvantage.\n- `bg roll 2d20k1`: rolls 2 d20 and keeps \
             the highest result, i.e. advantage.\n- `bg roll 10d6!`: rolls 10 d6 and explodes when a 6 is rolled.\n\
             - `bg roll d6+5`: rolls a d6 and adds 5.\n- `bg roll d6-4`: rolls a d6 and subtracts 4.\n\
-            - `bg roll d6|d8|d20`: rolls a d, a d8 and a d20. All above functionality is supported.\n')
-    async def roll(self, ctx, *roll_text):
-        '''
-        bg roll command
+            - `bg roll d6|d8|d20`: rolls a d, a d8 and a d20. All above functionality is supported.\n
         '''
         roller = Roller(''.join(roll_text))
         responses = roller.roll()
-        final_response = 'You rolled:\n'
+        await interaction.response.send_message('You rolled:\n')
+        final_response = ''
         for response in responses:
             if len(final_response) + len(response) > 1999:
-                await ctx.send(final_response)
+                await interaction.channel.send(final_response)
                 final_response = ''
             final_response += response
-        await ctx.send(final_response)
+        await interaction.channel.send(final_response)
 
     @commands.command(help='Lists the help for command category `dice`.',
                       pass_context=True)
